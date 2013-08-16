53fd986887a433c0d8145f62215f50a1d62404fa
Daniel M. Lofaro <dan@danlofaro.com>
2013-08-16 15:07:33 -0400

    first commit added valve get repo

---
+++ valve/get-repo-valve.sh
@@ -0,0 +1,8 @@
+mkdir repos
+cd repos
+git clone https://github.com/hubo/hubo_ros_core
+git clone https://github.com/hubo/hubo_ros_control
+git clone https://github.com/hubo/teleop_toolkit
+git clone https://github.com/hubo/localization_tools
+git clone https://github.com/hubo/drc_hubo
+git clone https://github.com/hubo/dynamixel_motor
