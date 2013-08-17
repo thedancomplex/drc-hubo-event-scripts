6c58d56c42101cf24ef755501ef97d31de2fefe1
Daniel M. Lofaro <geovana@hubo-ach-beta-3.(none)>
2013-08-17 16:15:47 -0400

    added roslaunch hubo_launch full_body_feedback_node.launch

--- run/valve/run-valve-2-body.sh
+++ run/valve/run-valve-2-body.sh
@@ -1,3 +1,4 @@
 sudo ls > /dev/null
 export ROS_MASTER_URI=http://192.168.0.212:11311/
-sudo service hubo-motion
+sudo service hubo-motion start
+roslaunch hubo_launch full_body_feedback_node.launch
