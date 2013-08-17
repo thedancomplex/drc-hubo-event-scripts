3462b85fd5eec30ef937ece289d0c75ece785208
Daniel M. Lofaro <dan@danlofaro.com>
2013-08-17 01:51:30 -0400

    added install defaults and valve

---
+++ install-defaults.sh
@@ -0,0 +1,7 @@
+HUBO_INSTALL_ROOT=$('pwd')
+cd defaults
+rm -R -f repos
+./install-dep.sh
+./install-hubo-ach.sh
+./install-hubo-motion-rt.sh
+cd $HUBO_INSTALL_ROOT
---
+++ install-valve.sh
@@ -0,0 +1,4 @@
+HUBO_INSTALL_ROOT=$('pwd')
+cd valve
+./install-valve.sh
+cd $HUBO_INSTALL_ROOT
